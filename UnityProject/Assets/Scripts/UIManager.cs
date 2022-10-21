f6c97a345e3ed121cc4d3b1a2b19b9bbaca94734
Michael_Kim <un8265@gmail.com>
2022-10-21 16:32:39 +0900

    GameScene Work

---
+++ UnityProject/Assets/Scripts/UIManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameMgr.Instance.Temp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
