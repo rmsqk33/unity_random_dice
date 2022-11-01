584f82ea642ac888f296d10e091454b6ed411741
Michael_Kim <un8265@gmail.com>
2022-11-01 19:38:21 +0900

    GameScene
    Change UI System, Set Resolution

--- UnityProject/Assets/Scripts/UIManager.cs
+++ UnityProject/Assets/Scripts/UIManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIManager : MonoBehaviour
+public class UIManager : Singleton<UIManager>
 {
     // Start is called before the first frame update
     void Start()
